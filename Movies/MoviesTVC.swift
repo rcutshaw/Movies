6ea71c924c612ae7066e5726793daeab21909664
Randy Cutshaw <rcutshaw@bitsmartz.com>
2016-07-31 14:21:29 -0500

    Completed Part-13 - added code in reachabilityStatusChanged() and added alert

--- Movies/MoviesTVC.swift
+++ Movies/MoviesTVC.swift
@@ -19,15 +19,6 @@ class MoviesTVC: UITableViewController {
         
         reachabilityStatusChanged()
         
-        let api = APIManager()
-        
-        //        api.loadData("https://itunes.apple.com/us/rss/topmovies/limit=10/genre=4401/json") {
-        //            (result:String) in
-        //            print(result)
-        //        }
-        
-        api.loadData("https://itunes.apple.com/us/rss/topmovies/limit=50/genre=4401/json",  // step 1
-            completion: didLoadData)  // when done, executes didLoadData  // step 2
     }
     
     func didLoadData(movies: [Movies]) {  // result from APIManager method is now the input to didLoadData - step 8
@@ -65,15 +56,54 @@ class MoviesTVC: UITableViewController {
         
         switch reachabilityStatus {
             
-        case NOACCESS : view.backgroundColor = UIColor.redColor()
-        //displayLabel.text = "No Internet"
-        case WIFI : view.backgroundColor = UIColor.greenColor()
-        //displayLabel.text = "Reachable with WIFI"
-        case WWAN : view.backgroundColor = UIColor.yellowColor()
-        //displayLabel.text = "Reachable with Cellular"
-        default:return
+        case NOACCESS :
+            view.backgroundColor = UIColor.redColor()
+            // Move back to main queue because viewDidLoad hasn't finished and no view on screen yet, so get the
+            // console Warning message "Presenting view controllers on detached view controllers is discouraged"
+            // Dispatching to main queue fixes this because it asynchronously runs this code, giving view time to appear first
+            dispatch_async(dispatch_get_main_queue()) {
+                let alert = UIAlertController(title: "No Internet Access", message: "Please make sure you are connected to the Internet", preferredStyle: .Alert)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .Default) {
+                    action -> () in
+                    print("Cancel")
+                }
+                
+                let deleteAction = UIAlertAction(title: "Delete", style: .Destructive) {
+                    action -> () in
+                    print("Delete")
+                }
+                
+                let okAction = UIAlertAction(title: "Ok", style: .Default) {
+                    action -> () in
+                    print("Ok")
+                    
+                    // do something if you want
+                    //alert.dismissViewControllerAnimated(true, completion: nil)
+                }
+                
+                alert.addAction(okAction)
+                alert.addAction(cancelAction)
+                alert.addAction(deleteAction)
+                
+                self.presentViewController(alert, animated: true, completion: nil)
+            }
             
+        default:
+            view.backgroundColor = UIColor.greenColor()
+            if movies.count > 0 {
+                print("do not refresh API")
+            } else {
+                runAPI()
+            }
         }
+    }
+    
+    func runAPI() {
+        // Call API
+        let api = APIManager()
+        api.loadData("https://itunes.apple.com/us/rss/topmovies/limit=50/genre=4401/json",
+                     completion: didLoadData)  // when done, executes didLoadData
         
     }
     
