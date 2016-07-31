41ec1447e295c7b64a993d77b812554c14e6ee1c
Randy Cutshaw <rcutshaw@bitsmartz.com>
2016-07-31 10:57:13 -0500

    Completed Part-9 - green, red, yellow for reachability status - changes to mainStoryboard, ViewController and AppDelegate - need to fix startup when no internet.

--- Movies/ViewController.swift
+++ Movies/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
 
     var movies = [Movies]()  // created this array to hold all our fetched movies
     
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(reachabilityStatusChanged), name: "ReachStatusChanged", object: nil)
+        
+        reachabilityStatusChanged()
         
         let api = APIManager()
         
@@ -53,5 +59,29 @@ class ViewController: UIViewController {
         //            print("\(i) name = \(video.vName)")
         //        }
     }
+    
+    func reachabilityStatusChanged()
+    {
+        
+        switch reachabilityStatus {
+            
+        case NOACCESS : view.backgroundColor = UIColor.redColor()
+        displayLabel.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+        displayLabel.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+        displayLabel.text = "Reachable with Cellular"
+        default:return
+            
+        }
+        
+    }
+    
+    // Is called just as the object is about to be deallocated
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
+    
 }
 
