2cfb6d982f381b07867f4ae9b258e4b2f7095992
Randy Cutshaw <rcutshaw@bitsmartz.com>
2016-08-04 17:15:27 -0500

    Completed Part-24

---
+++ Movies/AboutVC.swift
@@ -0,0 +1,18 @@
+//
+//  AboutVC.swift
+//  Movies
+//
+//  Created by David Cutshaw on 8/4/16.
+//  Copyright © 2016 Bit Smartz LLC. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
